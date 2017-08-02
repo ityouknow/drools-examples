bc31e8f9ed399464ba182dbcf47b4d1ab6a510b2
ityouknow <ityouknow@126.com>
2017-08-02 12:05:36 +0800

    m

--- drools-simple-demo/src/main/java/com/neo/drools/DroolsScoreExample.java
+++ drools-simple-demo/src/main/java/com/neo/drools/DroolsScoreExample.java
@@ -11,7 +11,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DroolsRules {
+public class DroolsScoreExample {
 
 
     /**
--- drools-simple-demo/src/main/java/com/neo/drools/JavaScoreExample.java
+++ drools-simple-demo/src/main/java/com/neo/drools/JavaScoreExample.java
@@ -8,7 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JavaRules {
+public class JavaScoreExample {
       
     public static void main(String[] args) throws Exception {  
           
